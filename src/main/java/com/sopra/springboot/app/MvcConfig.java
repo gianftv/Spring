a203747495455320979f9e6d327d43a8dc336125
Arranque 4 <Arranque 4@a18-pc10>
2018-12-10 18:20:56 +0100

    fotos

---
+++ src/main/java/com/sopra/springboot/app/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.sopra.springboot.app;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class MvcConfig  extends WebMvcConfigurerAdapter{
+	{
+}
+
+}
--- src/main/java/com/sopra/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/sopra/springboot/app/controllers/ClienteController.java
@@ -107,8 +107,12 @@ public class ClienteController {
 		}
 		
 		if(!foto.isEmpty()) {
-			Path directorioRecursos = Paths.get("src/main/resources/static/uploads");
-			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			//APUNTANDO AL WORKSPACE
+//			Path directorioRecursos = Paths.get("src/main/resources/static/uploads");
+//			String rootPath = directorioRecursos.toFile().getAbsolutePath();
+			
+			//APUNTANDO A DIRECTORIO EXTERNO - OPCION CORRECTA
+			String rootPath="C://temp//uploads";
 			try {
 				byte[] bytes= foto.getBytes();
 				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
