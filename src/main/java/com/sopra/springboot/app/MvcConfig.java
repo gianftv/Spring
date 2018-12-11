7a3b873c712aac1323d686d91e0092df672e23bb
Arranque 4 <Arranque 4@192.168.5.115>
2018-12-11 12:13:16 +0100

    loggeo

--- src/main/java/com/sopra/springboot/app/MvcConfig.java
+++ src/main/java/com/sopra/springboot/app/MvcConfig.java
@@ -1,13 +1,31 @@
 package com.sopra.springboot.app;
 
+import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
 @Configuration
-public class MvcConfig  extends WebMvcConfigurerAdapter{
-	{
-}
+public class MvcConfig extends WebMvcConfigurerAdapter{
+	
+	private final Logger log=LoggerFactory.getLogger(getClass());
+
+	//@Override
+	//public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+	//	super.addResourceHandlers(registry);
+		
+		//USANDO EL METODO COPY DEFINIDOS DE ESTA MANERA EL GUARDADO EN EL PROYECTO
+		//String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		//log.info(resourcePath);
+		//registry.addResourceHandler("/uploads/**")
+		//.addResourceLocations("file:/C:/TEMP/uploads/");
+		//.addResourceLocations(resourcePath);
+		
+	//}
+	
 
 }
--- src/main/java/com/sopra/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/sopra/springboot/app/controllers/ClienteController.java
@@ -2,17 +2,25 @@ package com.sopra.springboot.app.controllers;
 
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -37,6 +45,28 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	private final Logger log=LoggerFactory.getLogger(getClass());
+	
+	@GetMapping(value="/uploads/{filename: .+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
+		
+		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		log.info("pathFoto: " + pathFoto);
+		Resource recurso = null;
+		try {
+			 recurso = new UrlResource(pathFoto.toUri());
+			 if(recurso.exists() || !recurso.isReadable()) {
+				 throw new RuntimeException ("Error: no se puede cargar la imagen: " + pathFoto.toString());
+			 }
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+recurso.getFilename() +"\"").body(recurso);
+	}
+	
+	
 	
 	@GetMapping(value="/ver/{id}")//casi lo mismo que request
 	public 	String ver(@PathVariable (value="id") Long id, Map<String, Object> model, RedirectAttributes flash){
@@ -111,13 +141,34 @@ public class ClienteController {
 //			Path directorioRecursos = Paths.get("src/main/resources/static/uploads");
 //			String rootPath = directorioRecursos.toFile().getAbsolutePath();
 			
-			//APUNTANDO A DIRECTORIO EXTERNO - OPCION CORRECTA
-			String rootPath="C://temp//uploads";
+			//APUNTANDO A DIRECTORIO EXTERNO - OPCION SOLO USADA CON SPRING Y TOCANDO EL RESOURCE HANDLER EN LA CLASE MVC CONFIG
+			//String rootPath="C://Temp//uploads";
+			
+			
+			
+			//APUNTANDO A DIRECTORIO EXTERNO - OPCION MAS COMUN USANDO UN UNIQUE FILENAME
+			String uniqueFilename = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();//nombre de la foto con numero random pasado a string
+			//path relativo al proyecto
+			Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
+			//path absoluto
+			Path rootAbsolutPath = rootPath.toAbsolutePath();
+			//usamos la clase LogFactory para ber por consola nuestras rutas
+			log.info("rootPath: " + rootPath);
+			log.info("rootAbsolutPath: " + rootAbsolutPath);
+			
+			
 			try {
-				byte[] bytes= foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
-				Files.write(rutaCompleta, bytes);
-				flash.addFlashAttribute("info", "Ha subido correctamente '" + foto.getOriginalFilename()+ "'");
+//				USANDO write Y PASANDOLO COMO bytes
+//				byte[] bytes= foto.getBytes();
+//				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
+//				Files.write(rutaCompleta, bytes);
+				
+//				MISMA OPERACION CON EL METODO COPY
+				Files.copy(foto.getInputStream(), rootAbsolutPath);//foto del multiartfile, copia la foto a roorAbsolutePath
+				
+				
+				
+				flash.addFlashAttribute("info", "Ha subido correctamente '" + uniqueFilename + "'");
 				
 				cliente.setFoto(foto.getOriginalFilename());
 			}catch (IOException e) {
